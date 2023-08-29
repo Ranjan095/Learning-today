df33e7291fcccc8eeb1818d554c314583256a921
Ranjan Yadav <113471389+Ranjan095@users.noreply.github.com>
2023-08-29 14:35:52 +0530

    updated with hover

--- Chai_and_code/Chai-or-React/01Project-bg/src/App.jsx
+++ Chai_and_code/Chai-or-React/01Project-bg/src/App.jsx
@@ -6,19 +6,22 @@ import viteLogo from "/vite.svg";
 
 function App() {
   const [color, setColor] = useState("gray");
+  const [isHover, setIsHover] = useState(false);
   let arrButton = ["red", "green", "white", "blue", "black"];
   return (
     <>
       <div style={{ backgroundColor: color }} className=" h-screen top-0">
-        <div className=" flex flex-wrap fixed gap-6 justify-center bg-white rounded-full top-2 inset-x-2 p ">
+        <div className=" flex flex-wrap fixed gap-6 justify-center bg-orange-400 rounded-full top-2 inset-x-2 p ">
           {arrButton.map((ele, i) => (
             <button
               style={{
-                backgroundColor: ele,
-                color: ele === "white" ? "black" : "white",
+                backgroundColor: isHover === ele ? "gray" : ele,
+                color: ele==='white'?"black":"white",
               }}
               className=" py-1 rounded-lg my-2 px-3 border text-base"
               onClick={() => setColor(ele)}
+              onMouseEnter={() => setIsHover(ele)}
+              onMouseLeave={() => setIsHover(false)}
               key={i + 1}
             >
               {ele[0].toUpperCase() + ele.slice(1)}
